907aca93efcef4605fcd4813e96e714c464bb280
cwj <595842570@qq.com>
2023-11-07 21:13:20 +0800

    feat: 更新包 (story#000)

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import { Suspense } from 'react';
 import { Provider } from 'react-redux';
 import { RouterProvider } from 'react-router-dom';
 
-import { init } from 'cwj_monitoring';
+import { TYPES, init } from 'cwj_monitoring';
 import ReactDOM from 'react-dom/client';
 
 import FullScreenLoading from '@/components/layout/loading';
@@ -18,8 +18,7 @@ import './utils/observer';
 //自己的npm包，数据埋点
 init({
   url: `${process.env.REACT_APP_AUTH_URL}/track`, //必传参数，数据上传服务器地址
-  max: 2, //可选参数，最大缓存数，即超过缓存数立即上传，默认为5
-  router: true, //可选参数，是否监听路由事件，默认关闭
+  plugin: [TYPES.ROUTER], //监听路由
   data: {
     vs: 'react-manage',
   },
